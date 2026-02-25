a17e512cb85212ebac226a22f7a4ee1569dc8466
vishyte <vishyte02@gmail.com>
2026-02-25 18:51:34 +0800

    updating

--- my-mongodb-app/.next/dev/server/pages/_app.js
+++
@@ -1,4 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_app.js")
-R.c("server/chunks/ssr/[root-of-the-server]__c680eada._.js")
-R.m("[project]/my-mongodb-app/pages/_app.tsx [ssr] (ecmascript)")
-module.exports=R.m("[project]/my-mongodb-app/pages/_app.tsx [ssr] (ecmascript)").exports
--- my-mongodb-app/.next/dev/server/pages/_document.js
+++
@@ -1,5 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_document.js")
-R.c("server/chunks/ssr/f2624_84412acb._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__6e589d2c._.js")
-R.m("[project]/my-mongodb-app/pages/_document.tsx [ssr] (ecmascript)")
-module.exports=R.m("[project]/my-mongodb-app/pages/_document.tsx [ssr] (ecmascript)").exports
