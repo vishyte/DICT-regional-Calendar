0bda0b5d18a784bb9f628587ba11e07b1da70429
vishyte <vishyte02@gmail.com>
2026-02-25 21:00:33 +0800

    updating backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,7 +9,7 @@ import activityRoutes from './routes/activities';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(helmet());
